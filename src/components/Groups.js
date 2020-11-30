fb92644aa6fe65834a0c1819e7a8fdcbdae8396c
cTraymany <chindalatht@gmail.com>
2020-11-30 14:53:26 -0800

    maps through groups to render each component

--- src/components/Groups.js
+++ src/components/Groups.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import Group from './Group'
+// import Group from './Group'
+import {Link} from 'react-router-dom'
 
 const Groups = (props) => {
-
     return(
         <div>
-            {props.groups.map(group => <h2 key={group.id}><Group group={group}/></h2>)}
+            {props.groups.map(group =>
+                <h3 key={group.id}>
+                    <Link to={`/groups/${group.name}`}>{group.name}</Link>
+                </h3>)}
         </div>
     )
 }
