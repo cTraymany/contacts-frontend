7710c3b4044e5dc744f22252af9b66c4d8692228
cTraymany <chindalatht@gmail.com>
2020-11-30 09:36:02 -0800

    updates groups component to display group component

--- src/components/Groups.js
+++ src/components/Groups.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Group from './Group'
 
 const Groups = (props) => {
 
     return(
         <div>
-            {props.groups.map(group => <h2 key={group.id}>{group.name}</h2>)}
+            {props.groups.map(group => <h2 key={group.id}><Group group={group}/></h2>)}
         </div>
     )
 }
