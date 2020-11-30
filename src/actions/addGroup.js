c3b7dd77ba1eeb9dd4b7926aaa03d66f3cbe779c
cTraymany <chindalatht@gmail.com>
2020-11-30 07:13:21 -0800

    creates group form, begins action creator for adding a new group

---
+++ src/actions/addGroup.js
@@ -0,0 +1,5 @@
+export const addGroup = (data) => {
+    return (dispatch) => {
+        
+    }
+}
--- src/components/GroupInput.js
+++ src/components/GroupInput.js
@@ -1,14 +1,32 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {addGroup} from '../actions/addGroup'
 
 class GroupInput extends React.Component {
 
+    state = {
+        name: ""
+    }
+
+    onChange = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    onSubmit = (event) => {
+       
+    }
+
     render() {
         return(
-            <div>
-                GroupInput
-            </div>
+            <form onSubmit={this.onSubmit}>
+                <label>Create a Group: </label>
+                <input onChange={this.onChange} type="text" placeholder="Group Name" value={this.state.name} ></input>
+                <input type="submit"></input>
+            </form>
         )
     }
 }
 
-export default GroupInput
+export default connect(null, {addGroup})(GroupInput)
