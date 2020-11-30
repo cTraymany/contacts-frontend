1c3fd8c3991e04a00d80f3f96d8edcec7c277f8c
cTraymany <chindalatht@gmail.com>
2020-11-30 15:03:57 -0800

    creates new contact form

---
+++ src/components/ContactInput.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default class ContactInput extends React.Component {
+
+    render() {
+        return(
+            <form>
+                <input type="text"></input>
+                <input type="submit"></input>
+            </form>
+        )
+    }
+}
