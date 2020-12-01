bdef754096babd84debdcf9b07db270dd9145d8b
cTraymany <chindalatht@gmail.com>
2020-11-30 16:45:27 -0800

    creates Contacts component and renders it along with contact input

--- src/containers/ContactsContainer.js
+++ src/containers/ContactsContainer.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import ContactInput from '../components/ContactInput'
+import Contacts from '../components/Contacts'
+
 
 export default class ContactsContainer extends React.Component {
 
     render() {
         return(
             <div>
-                ContactsContainer
+                <ContactInput />
+                <Contacts />
             </div>
         )
     }
