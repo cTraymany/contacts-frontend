bfd20b78f4af5e781be4a7470e9f561cf46d4b16
cTraymany <chindalatht@gmail.com>
2020-12-04 18:28:01 -0800

    debugs contact not rendering, adds event listener to delete contact

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,15 +1,24 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {deleteContact} from '../actions/deleteContact'
 
 const Contacts = (props) => {
-    console.log(props)
+    // console.log(props)
+
+    const deleteContact = (contact) => {
+        // debugger
+        props.deleteContact(contact.attributes.group_id, parseInt(contact.id))
+    }
+
     return(
         <div>
             {props.contacts && props.contacts.map(contact => {
+                // debugger
                 return(
                     <div>
-                        <h3 key={contact.id}>{contact.attributes.name}</h3>
-                        <p>{contact.attributes.phoneNumber}</p>
-                        <p>{contact.attributes.birthday}</p>
+                        <h3 key={contact.id}>{contact.attributes && contact.attributes.name} <button onClick={() => deleteContact(contact)}>X</button></h3>
+                        <p>{contact.attributes && contact.attributes.phoneNumber}</p>
+                        <p>{contact.attributes && contact.attributes.birthday}</p>
                     </div>
                 )
             })}
@@ -17,6 +26,6 @@ const Contacts = (props) => {
     )
 }
 
-export default Contacts
+export default connect(null, {deleteContact})(Contacts)
 
 // CHANGE TO RENDER CONTACT COMPONENT
