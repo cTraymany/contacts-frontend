0a555153ea7794a1939f3b68b5168d18dad9ad14
cTraymany <chindalatht@gmail.com>
2020-12-05 16:50:02 -0800

    moves key to child div

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,20 +3,17 @@ import {connect} from 'react-redux'
 import {deleteContact} from '../actions/deleteContact'
 
 const Contacts = (props) => {
-    // console.log(props)
 
     const deleteContact = (contact) => {
-        // debugger
         props.deleteContact(contact.attributes.group_id, parseInt(contact.id))
     }
 
     return(
         <div>
             {props.contacts && props.contacts.map(contact => {
-                // debugger
                 return(
-                    <div>
-                        <h3 key={contact.id}>{contact.attributes && contact.attributes.name} <button onClick={() => deleteContact(contact)}>X</button></h3>
+                    <div key={contact.id}>
+                        <h3>{contact.attributes && contact.attributes.name} <button onClick={() => deleteContact(contact)}>X</button></h3>
                         <p>{contact.attributes && contact.attributes.phoneNumber}</p>
                         <p>{contact.attributes && contact.attributes.birthday}</p>
                     </div>
