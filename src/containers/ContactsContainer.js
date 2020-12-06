e1e193c0789bb13141bc93e6f302d758b1f1f774
cTraymany <chindalatht@gmail.com>
2020-12-05 19:00:49 -0800

    moves contact input to switch in group container

--- src/containers/ContactsContainer.js
+++ src/containers/ContactsContainer.js
@@ -1,16 +1,11 @@
 import React from 'react'
-import ContactInput from '../components/ContactInput'
 import Contacts from '../components/Contacts'
 
 
 export default class ContactsContainer extends React.Component {
     render() {
-        console.log("contacts container", this.props.group)
         return(
-            <div>
-                <ContactInput group={this.props.group} />
-                <Contacts contacts={this.props.group && this.props.group.attributes.contacts} />
-            </div>
+            <Contacts contacts={this.props.group && this.props.group.attributes.contacts} />
         )
     }
 }
