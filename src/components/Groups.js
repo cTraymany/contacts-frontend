0bb62a4d0696a9cd6407bdbe4704a7c11704a278
cTraymany <chindalatht@gmail.com>
2020-12-05 19:00:14 -0800

    fixes rendering issue

--- src/components/Groups.js
+++ src/components/Groups.js
@@ -5,9 +5,9 @@ import {Link} from 'react-router-dom'
 const Groups = (props) => {
     return(
         <div>
-            {props.groups.data.map(group =>
+            {props.groups.data && props.groups.data.map(group =>
                 <h3 key={group.id}>
-                    <Link to={`/groups/${group.attributes.name.toLowerCase()}`}>{group.attributes.name}</Link>
+                    <Link to={`/groups/${group.attributes && group.attributes.name.toLowerCase()}`}>{group.attributes && group.attributes.name}</Link>
                 </h3>)}
         </div>
     )
