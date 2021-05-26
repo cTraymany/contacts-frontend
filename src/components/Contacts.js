7702ecbc1040bf9c03c5d6b637f29c5d8d095221
Chindalath <chindalatht@gmail.com>
2021-05-25 17:39:12 -0700

    passes deleteContact down to contact component

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -3,13 +3,14 @@ import {connect} from 'react-redux'
 import {deleteContact} from '../actions/deleteContact'
 import {Link} from 'react-router-dom'
 import Button from 'react-bootstrap/Button'
-import Contact from './Contact'
+import Contact from './contact'
+// The above should be capital "Contact" but errors occur if so
 
 const Contacts = (props) => {
     return(
         <>
             {props.contacts && props.contacts.map(contact => (
-                <Contact contact={contact}/>
+                <Contact contact={contact} deleteContact={(groupId, contactId) => props.deleteContact(groupId, contactId)}/>
             ))}
             <Link to={`/groups/${props.match.params.name}/new`}><Button>Add Contact</Button></Link>
         </>
